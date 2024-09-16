fb8bbca265f66ab4bc675bd62089eebbc42e36cf
tratchapong <tratchapong@gmail.com>
2024-09-16 17:31:08 +0700

    start with facebook layout

---
+++ src/App.jsx
@@ -0,0 +1,75 @@
+function App() {
+
+  return (
+    <div className='min-h-screen bg-yellow-100'>
+      <header className="bg-orange-200 h-14 w-full fixed top-0 z-10">Header </header>
+      <main className="relative flex bg-blue-200 border h-screen pt-14">
+        <div className="w-[280px] bg-red-200 overflow-auto fixed top-14 h-full">
+          <div className="btn btn-info w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+
+        </div>
+        <div className="px-[280px] w-full">
+          <div className="artboard phone-1 my-4 border mx-auto">320×568</div>
+          <div className="artboard phone-1 my-4 border mx-auto">320×568</div>
+          <div className="artboard phone-1 my-4 border mx-auto">320×568</div>
+          <div className="artboard phone-1 my-4 border mx-auto">320×568</div>
+          <div className="artboard phone-1 my-4 border mx-auto">320×568</div>
+          <div className="artboard phone-1 my-4 border mx-auto">320×568</div>
+
+        </div>
+        <div className="w-[280px] bg-red-200 overflow-auto fixed top-14 right-0 h-full">
+          <div className="btn btn-info w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+          <div className="btn btn-primary w-4/5">Menu1</div>
+
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default App
